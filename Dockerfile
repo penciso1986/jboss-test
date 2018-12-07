6d8988558e7cd4bcda526ae77f0a6bbd33d18d9f
Pablo Enciso <pablorodrigo.enciso@gmail.com>
2018-12-07 03:50:00 -0300

    updates y agregado de jdk1.7

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,15 @@
-FROM debian:latest
+FROM ubuntu:latest
 
 RUN apt-get update && apt-get upgrade
 
-RUN apt-get install openjdk-8-jre-headless net-tools procps -y
+RUN mkdir /opt/webapps 
 
-RUN mkdir /opt/jboss-7.1.1
+COPY ./ /opt/webapps
 
-COPY ./jboss-7.1.1/ /opt/jboss-7.1.1/
+RUN chmod +x /opt/webapps/jboss-7.1.1/bin/standalone/standalone.sh
 
-RUN chmod +x /opt/jboss-7.1.1/bin/standalone.sh
+CMD ["/opt/jboss-7.1.1/bin/standalone/standalone.sh"]
 
-CMD ["/opt/jboss-7.1.1/bin/standalone.sh"]
-
-EXPOSE 8080:8080
+EXPOSE 8180:8180
 
 
--- jboss-7.1.1/bin/standalone/standalone.sh
+++ jboss-7.1.1/bin/standalone/standalone.sh
@@ -4,18 +4,18 @@ JAVA_OPTS="$JAVA_OPTS -Xmx256m"
 JBOSS_CLASSPATH="$JBOSS_CLASSPATH:"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseCompressedOops"
 DISPLAY=:0
-#JAVA_HOME="/Appweb/Oracle_Java-1.8"
-#JDK_TOOLS="$JAVA_HOME/lib/tools.jar"
+JAVA_HOME="/opt/webapps/jboss-test/jre1.7.0_80"
+JDK_TOOLS="$JAVA_HOME/lib/tools.jar"
 export DISPLAY
 export PATH
 export JAVA_OPTS
 export JBOSS_CLASSPATH
-#export JAVA_HOME
-#export JDK_TOOLS
-cd /opt/jboss-7.1.1/standalone
-rm -Rf /opt/jboss-7.1.1/standalone/log
-rm -Rf /opt/jboss-7.1.1/standalone/work
-rm -Rf /opt/jboss-7.1.1/standalone/tmp
+export JAVA_HOME
+export JDK_TOOLS
+cd /opt/webapps/jboss-7.1.1/standalone
+rm -Rf /opt/webapps/jboss-7.1.1/standalone/log
+rm -Rf /opt/webapps/jboss-7.1.1/standalone/work
+rm -Rf /opt/webapps/jboss-7.1.1/standalone/tmp
 sleep 10
 
-/opt/jboss-7.1.1/bin/standalone.sh --server-config=standalone.xml -Djboss.server.base.dir=/opt/jboss-7.1.1/standalone -b 0.0.0.0 -bmanagment 0.0.0.0 > /dev/null &
+/opt/webapps/jboss-7.1.1/bin/standalone.sh --server-config=standalone.xml -Djboss.server.base.dir=/opt/webapps/jboss-7.1.1/standalone -b 0.0.0.0 -bmanagment 0.0.0.0 > /dev/null &
